f1bec4b0ea439cd6edeb177c219533621cd37565
Jonathon Hellmann <jonatho7@vt.edu>
2015-01-20 16:44:19 -0500

    Added 3 Size Buttons for Google Map

--- controlScripts.js
+++ controlScripts.js
@@ -12,6 +12,24 @@ var main = function() {
         document.getElementById("map-canvas").style.width = "" + width + "px";
         document.getElementById("map-canvas").style.height = "" + height + "px";
     });
+
+    $(".changeMapSize1").click(function () {
+        document.getElementById("map-canvas").style.width = "640px";
+        document.getElementById("map-canvas").style.height = "360px";
+    });
+
+    $(".changeMapSize2").click(function () {
+        document.getElementById("map-canvas").style.width = "864px";
+        document.getElementById("map-canvas").style.height = "486px";
+    });
+
+    $(".changeMapSize3").click(function () {
+        document.getElementById("map-canvas").style.width = "1024px";
+        document.getElementById("map-canvas").style.height = "576px";
+    });
+
+
+
 };
 
 $(document).ready(main);
