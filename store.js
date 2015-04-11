4e630dfa9cf2913002d3a78b5a7ca8fad8f84e3b
Jonathon Hellmann <jonatho7@vt.edu>
2015-04-10 22:26:44 -0400

    white and grey scriptsPaneTexture

--- store.js
+++ store.js
@@ -842,7 +842,7 @@ SnapSerializer.prototype.loadScripts = function (scripts, model) {
     var myself = this,
         scale = SyntaxElementMorph.prototype.scale;
     //scripts.texture = 'scriptsPaneTexture.gif';
-    scripts.texture = 'static/Snap/scriptsPaneTexture.gif';
+    scripts.texture = 'static/Snap/scriptsPaneTexture.png';
     model.children.forEach(function (child) {
         var element;
         if (child.tag === 'script') {
